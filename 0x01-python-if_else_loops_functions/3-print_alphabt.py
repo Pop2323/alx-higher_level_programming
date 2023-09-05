dfc55d8538acce055d65cd29bda4f973f736fe23
Pop2323 <omar4500455@gmail.com>
2023-09-05 08:54:56 -0700

    3-print_alphabt

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alph in range(ord("a"), ord("z") +1):
-    if alph != ord('e') and chr != ord('q'):
+for alph in range(ord("a"), ord("z") + 1):
+    if chr != ord('e') and chr != ord('q'):
         print("{:c}".format(alph), end="")
