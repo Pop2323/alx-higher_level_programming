8b049c79c0d609b1605dc88dd6e646dc96b3c5e5
Pop2323 <omar4500455@gmail.com>
2023-09-05 09:50:15 -0700

    100-print_tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for alph in range(122, 96, -1):
-    print(chr(alph) if (alph % 2 == 0) else chr(alph - 32), end='')
-print()
-
+for alph in range(ord("z"), ord("a") - 1, -1):
+    if alph % 2 == 0:
+        char = 0
+    else:
+        char = 32
+    print("{}".format(chr(alph - char)), end="")
