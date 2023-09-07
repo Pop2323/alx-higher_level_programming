08b48f2110b40ad7ad2266b6124439bf23f458d8
Pop2323 <omar4500455@gmail.com>
2023-09-07 11:23:12 -0700

    100-my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div 
     import sys
-
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
-
     if op == '+':
         print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
     elif op == '-':
