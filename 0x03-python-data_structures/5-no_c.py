699335a564a8bdceedeb41757cd792f782ac2e39
Pop2323 <omar4500455@gmail.com>
2023-09-10 15:58:38 -0700

    5-no_c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    replace_str = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            replace_str += char
+    return replace_str
