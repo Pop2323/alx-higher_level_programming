7aff72b25dfac3dbb782c292ea4508a37ea0fb29
Pop2323 <omar4500455@gmail.com>
2023-09-13 17:51:35 -0700

    100-weight_average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None or my_list == []:
+        return 0
+    set1 = 0
+    set2 = 0
+    for x, y in my_list:
+        set1 += (x *y)
+        set2 += y
+    return set1 / set2
