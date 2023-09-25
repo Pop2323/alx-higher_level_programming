4cb1aa888127a8a8326e55ad89eb29dec9f9cce9
Pop2323 <omar4500455@gmail.com>
2023-09-25 05:25:27 -0700

    102-magic_calculation

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    res = 0
+    for n in range(0, 4):
+        try:
+            if n > a:
+                raise Exception("Too far")
+            res = (a ** b) / n
+        except Exception as e:
+            res = a + b
+            break
+    return res
