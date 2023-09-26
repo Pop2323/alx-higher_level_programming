82b25834b190787d38499d52f64aa58e340db422
Pop2323 <omar4500455@gmail.com>
2023-09-26 07:43:19 -0700

    2_commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Define a class called Square"""
+
+class Square:
+    """Represent a Square"""
+
+    def __init__(self, size):
+        """Initialize a Square
+        Args:
+            size(int): The size of the square
+        """
+        self.__size = size
