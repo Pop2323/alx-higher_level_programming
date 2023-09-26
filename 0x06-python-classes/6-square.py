0e31d4fa345ed9e650ff16a2070cf726c1990ca8
Pop2323 <omar4500455@gmail.com>
2023-09-26 10:07:18 -0700

    7_commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,6 +44,7 @@ class Square:
     def area(self):
         """Public instance method to calculate the area"""
         return (self.__size * self.__size)
+
     def my_print(self):
         """Public instance method to print the square"""
         if self.__size == 0:
