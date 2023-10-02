4341d670f93fba5291f621be9b30c9b27eccef2e
Pop2323 <omar4500455@gmail.com>
2023-10-02 11:44:53 -0700

    1-rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+
+"""define the calss Rectangle"""
+
+
+class Rectangle:
+    """represent Rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initialize the instance with optional width and height
+        
+        Args:
+            width: represent the rectangle width
+            height: represent the rectangle width
+        
+        Raises:
+            TypeError: if width and height is not integer
+            ValueError: if width and height less than zero
+        """
+        self.width = width
+        self.height = height
+        
+        @property
+        def width(self):
+            """Define the property for width"""
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """Define the setter for width"""
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+            
+        @property
+        def height(self):
+            """Define the property for width"""
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            """Define the setter for height"""
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
