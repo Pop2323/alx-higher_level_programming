bc24f4b14c6fb108fc2acc173573012ee37ff516
Pop2323 <omar4500455@gmail.com>
2023-10-02 11:42:08 -0700

    My first commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+"""class that will define the Rectangle"""
+class Rectangle:
+    """ an empty class Rectangle that defines a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This is python-more_classes readme
