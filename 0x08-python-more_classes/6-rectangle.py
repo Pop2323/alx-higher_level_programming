de716daa4fc77ae47f0f7cc319c92e1de014ac14
Pop2323 <omar4500455@gmail.com>
2023-10-02 18:31:27 -0700

    6-rectangle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Define the setter for width"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -63,11 +63,10 @@ class Rectangle:
     def __str__(self) -> str:
         if self.__width == 0 or self.__height == 0:
             return ""
-        else:
-            shape = ""
-            for h in range(self.__height):
-                for w in range(self.__width):
-                    shape += "#"
+        shape = ""
+        for h in range(self.__height):
+            for w in range(self.__width):
+                shape += "#"
                 if h < self.__height -1:
                     shape += "\n"
             return (shape)
