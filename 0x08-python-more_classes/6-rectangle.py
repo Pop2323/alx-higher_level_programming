7b797e5393ce89f8ae0eccdd0ae429cc30847eff
Pop2323 <omar4500455@gmail.com>
2023-10-02 18:24:53 -0700

    6-rectangle

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+
+"""Define the class Rectangle"""
+
+
+class Rectangle:
+    """Represent Rectangle"""
+    instance_of_Rectangle = 0
+
+    def __init__(self, width=0, height=0):
+        """Initialize the instance with optional width and height
+        Args:
+            width: represent the rectangle width
+            height: represent the rectangle width
+        Raises:
+            TypeError: if width and height is not integer
+            ValueError: if width and height less than zero
+        """
+        self.width = width
+        self.height = height
+        Rectangle.instance_of_Rectangle += 1
+
+    @property
+    def width(self):
+        """Define the property for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Define the setter for width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Define the property for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Define the setter for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """return the rectangle area"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """return the  rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self) -> str:
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            shape = ""
+            for h in range(self.__height):
+                for w in range(self.__width):
+                    shape += "#"
+                if h < self.__height -1:
+                    shape += "\n"
+            return (shape)
+
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.instance_of_Rectangle -= 1
