72f0c909c808a56e5f54bb105bf43d460d21d97b
Pop2323 <omar4500455@gmail.com>
2023-10-06 17:51:02 -0700

    My first commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,20 +5,20 @@
 function that adds 2 integers.
 
 """
+
+
 def add_integer(a, b=98):
     """Return the result of the intger calculation
 
     Args:
         a: the first arg
         b: the second arg
-    
     Raises:
         TypeError: if a and b must be integers or floats
-    
     Return:
         the sum the two numbers
     """
-    
+
     if ((not isinstance(a, int)) and not isinstance(a, float)):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
