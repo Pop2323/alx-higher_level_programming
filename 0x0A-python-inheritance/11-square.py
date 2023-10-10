e482c8db876904b753a1c639edc9e3d02da39a65
Pop2323 <omar4500455@gmail.com>
2023-10-09 23:57:49 -0700

    11-square commit

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""this module inheritane a square from rectangle"""
+"""This module inherits a square from rectangle"""
 Rectangle = __import__('9-rectangle').Rectangle
-
 
 class Square(Rectangle):
     """Define a square"""
 
     def __init__(self, size):
-        """Initial a new square"""
-        self.integer_validator("size", size)
+        """Initialize a new square"""
+        super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
