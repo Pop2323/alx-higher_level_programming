b9611927f512f2be4cfa0bbf63906c256ce26dbf
Pop2323 <omar4500455@gmail.com>
2023-10-09 23:11:33 -0700

    100-my_int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,7 +5,10 @@
 
 class MyInt(int):
     def __eq__(self, value):
+        """Override the equality operator (==) to invert its behavior"""
         return super().__ne__(value)
 
     def __ne__(self, value):
+        """Override the inequality operator (!=) to invert its behavioe"""
         return super().__eq__(value)
+
