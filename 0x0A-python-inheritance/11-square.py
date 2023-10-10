7895b70368a3eea0b12ba63691dbd79b9f8f28c7
Pop2323 <omar4500455@gmail.com>
2023-10-09 23:51:01 -0700

    11-square commit

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,8 +7,7 @@ class Square(Rectangle):
     """Define a square"""
 
     def __init__(self, size):
-        """Initialize a square
-        """
+        """Initialize a square"""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
