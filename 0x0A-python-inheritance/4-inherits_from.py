76071cfef8a0ddb0ebbbac81dd04b97952f83980
Pop2323 <omar4500455@gmail.com>
2023-10-09 21:26:16 -0700

    4-inherits_from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+"""
+Func that check if the object is an instance of a class that inherited
+(directly or indirectly) from the specified class 
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if the object is an instance of a class that inherited from a specified class.
+    Args:
+    obj: The object to check.
+    a_class: The specified class.
+    Returns:
+    True if obj is an instance of a class that inherited from a_class, False otherwise.
+    """
+    return (isinstance(type(obj) == a_class) and type(obj) != a_class)
