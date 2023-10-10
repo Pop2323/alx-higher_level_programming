934629a69cce31502ff6bfe0c4bb0d3bb1a67699
Pop2323 <omar4500455@gmail.com>
2023-10-09 23:01:07 -0700

    100-my_int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""module class MyInt that inherits from int"""
 
+"""module class MyInt that inherits from int"""
 
 class MyInt(int):
     def __eq__(self, value):
-        return self.other != value
+        return super().__ne__(value)
 
     def __ne__(self, value):
-        return self.other == value
+        return super().__eq__(value)
