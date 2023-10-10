7ea9341e7a7d2e41d67fa0f2ea3452bc8947aaf1
Pop2323 <omar4500455@gmail.com>
2023-10-09 21:11:12 -0700

    my_list commit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""This module inherits from list class"""
+
+
+class MyList(list):
+    """ class MyList that inherits from list"""
+    def print_sorted(self):
+        """print the sorted list"""
+        print(sorted(self))
