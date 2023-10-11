f4026c99b0f594f30fbfa80f299ed6f355549273
Pop2323 <omar4500455@gmail.com>
2023-10-10 18:37:19 -0700

    3rd commit

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""a class Student that defines a student"""
+"""A class Student that defines a student"""
 
 
-def __init__(self, first_name, last_name, age):
-    """Initializes a new student"""
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-
-def to_json(self):
-    """ retrieves a dictionary representation of a Student"""
-    return self.__dict__
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student"""
+        return self.__dict__
