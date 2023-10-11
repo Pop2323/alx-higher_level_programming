32fa0d4d65cd0d00aad008447e20581a56757e85
Pop2323 <omar4500455@gmail.com>
2023-10-10 18:51:46 -0700

    3rd commit

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
     function that inserts a line of text to a file,
-    after each line containing a specific string
-"""
+    after each line containing a specific string"""
 
 
 def pascal_triangle(n):
-     if n <= 0:
+    """Pascal's Triangle of size n
+    """
+    if n <= 0:
         return []
 
     triangles = [[1]]
