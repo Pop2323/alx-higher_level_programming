230a9ac19657dbb814bf4799fdf2a37845364b19
Pop2323 <omar4500455@gmail.com>
2023-10-10 19:01:07 -0700

    3rd commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Adds all arguments to a Python list and then saves them to a file"""
 
-import sys
 
+import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
