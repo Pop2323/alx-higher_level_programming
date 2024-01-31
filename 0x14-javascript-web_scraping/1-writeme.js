94d1b96030feff7866850977055660e1b04810e6
Pop2323 <omar4500455@gmail.com>
2024-01-30 17:19:22 -0800

    javascript-web_scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const fileName = process.argv[2];
 const string = process.argv[3];
 
-fs.readFile(fileName, string, 'utf-8', (err) => {
+fs.writeFile(fileName, string, 'utf-8', (err) => {
   if (err) {
     console.log(err);
   }
